cd1b86f4dd315d618b7fcacd6dd8eab30d7205be
Daniel Alexander <davisd49@msu.edu>
2016-12-23 11:43:45 -0500

    bttons working

---
+++ scripts/scripts.js
@@ -0,0 +1,75 @@
+$(document).ready(function(){
+
+	var streamers=['docgotgame','freecodecamp','brunofin','gamecatt','ej_sa','comster_404'];
+
+	function getStreamerInfo(){
+
+		streamers.forEach(function(streamer){
+
+			function assembleURL(type, name){
+				return "https://wind-bow.gomix.me/twitch-api/" + type + "/" + name ;
+			}
+
+			$.ajax({
+				type: "GET",
+				url: (assembleURL("streams", streamer)),
+				//client-id provided but doesn't work
+				
+				success:function(data){
+
+					if(data.stream == null){
+						status = 'offline';
+						var content='';
+						$('#table').append("<tr class='target offline'><td class='streamer'><a target='blank' href='https://www.twitch.tv/"+ streamer +"'>" + streamer + "<br><span class='content'>" + content + "</span></a></td><td class='status'>" + status + "</td></tr>");
+					} else{
+						status = 'LIVE';
+						var content = '-streaming ' + data.stream.game + "-";
+						$('#table').append("<tr class='target online'><td class='streamer'><a target='blank' href='https://www.twitch.tv/"+ streamer +"'>" + streamer + "<br><span class='content'>" + content + "</span></a></td><td class='status'>" + status + "</td></tr>");
+					}
+					
+				
+					// console.log(JSON.stringify(data));
+
+					// if(data.stream == null){
+					// 	$('.target').addClass('offline');
+					// } else{
+					// 	$('.target').addClass('online');
+					// }
+				
+					$('#table').append(info);
+			
+				},
+				error:(function(){
+					alert('oops');
+				})
+
+			})
+		})
+	}
+
+	getStreamerInfo();
+
+	$('#navAll').click(function(){
+		$('.target').removeClass('hidden');
+	})
+
+	$('#navOn').click(function(){
+		$('.offline').addClass('hidden');
+		$('.online').removeClass('hidden');
+	})
+
+	$('#navOff').click(function(){
+		$('.online').addClass('hidden');
+		$('.offline').removeClass('hidden');
+	})
+	//pushing hardcoded urls into urls array
+	// var rootUrl="https://api.twitch.tv/kraken/streams/"
+	// var urls =[];
+
+	// for(i=0; i<streamers.length;i++){
+	// 	urls.push(rootUrl + streamers[i])
+	// 	/*console.log(JSON.stringify(urls));*/
+	// }
+	
+})
+//cijw4sis1i9zyhnjzuvmd0wkuvmjvo
